0a3e151618940a345a0f7c4ca7979cd67d744ea5
Alessandrosantos215 <117760649+Alessandrosantos215@users.noreply.github.com>
2024-09-24 15:26:06 -0300

    end of class 15

---
+++ Exercicios/Aulas/Aula15.py
@@ -0,0 +1,44 @@
+'''cont =1
+while True:
+    print(cont, '->', end='')
+    cont +=1
+print ('acabou')
+
+
+-----------******---------'''
+'''n= cont = 0
+while cont <3:
+    n = int(input('Digite um numero: '))
+    cont += 1
+    
+-------***********-------
+'''
+
+'''n = 0
+while n != 999:
+    n = int(input('Digite um numero: '))
+    
+    
+---------**********-------'''
+
+'''
+n = s = 0
+while True:
+    n = int(input('Digite um numero: '))
+    if n == 999:
+        break
+    s += n
+#print('A soma vale {}'.format(s))
+print(f'A soma vale {s}')
+
+---------------***********-----------'''
+'''
+nome = 'Jose'
+idade = 33
+print(f'O {} tem {} anos') #PHYTHON 3.6 +
+print('O {} tem {} anos.'.format(nome, idade)) # PYTHON 3
+print('O %s tem %d anos.' % (nome, idade)) # PYTHON 2
+
+
+VOMEÇA NO EXERCICIO [66]
+-------********-------------'''
